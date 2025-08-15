40b859d97abeddd6366130753d2b8b3b72238b12
Adishesha Sai Nandkoomar <ST10470336@vgconnect.edu.za>
2025-08-15 14:07:20 +0200

    Created the EmptyTile class.

---
+++ EmptyTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_6122_POE_Adishesha_and_Ayden
+{
+    public class EmptyTile: Tile
+    {
+        public EmptyTile(Position position) : base(position) { }
+
+        public override char Display
+        {
+            get { return '.'; }
+        }
+    }
+}
