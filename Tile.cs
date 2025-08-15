b75714a5d1e3020a3a726ba7dfe9dbf4f4758a81
Adishesha Sai Nandkoomar <ST10470336@vgconnect.edu.za>
2025-08-15 14:01:02 +0200

    Created the Tile class.

---
+++ Tile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_6122_POE_Adishesha_and_Ayden
+{
+    public abstract class Tile
+    {
+        private Position position;
+
+
+        public Tile(Position position)
+        {
+            this.position = position;
+        }
+
+        public int X
+        {
+            get { return position.X; }
+        }
+
+        public int Y
+        {
+            get { return position.Y; }
+        }
+
+        public Position Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public abstract char Display { get; }
+    }
+}
