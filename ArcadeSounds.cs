15150a9a169ad50752feeecb8d09cef70c6d7a7f
Adishesha Sai Nandkoomar <ST10470336@vgconnect.edu.za>
2025-08-22 14:13:55 +0200

    Implement ArcadeSounds class with comprehensive beep-based sound effects for movement, completion, restart, and game events

---
+++ ArcadeSounds.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_6122_POE_Adishesha_and_Ayden
+{
+    using System.Media;
+
+    public class ArcadeSounds
+    {
+        public static void PlayMoveSound()
+        {
+            // Simple beep for movement
+            Console.Beep(800, 50);
+        }
+
+        public static void PlayLevelComplete()
+        {
+            Console.Beep(1000, 200);
+            Console.Beep(1200, 200);
+            Console.Beep(1500, 400);
+        }
+    }
+}
